8e869016febb081eeb427a6514f209a2f572a483
Soufiane Sadgali <ssadgali2@gmail.com>
2024-07-30 17:47:45 +0100

    Task 11 - Weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,7 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   const counter = weakMap.get(endpoint) || 0;
 
-  if (counter >= 5) throw Error('Endpoint load is high');
+  if (counter >= 4) throw new Error('Endpoint load is high');
 
   weakMap.set(endpoint, counter + 1);
 }

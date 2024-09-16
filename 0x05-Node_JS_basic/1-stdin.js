630506020f7a808b544b10effdabb0324d495b1b
Soufiane Sadgali <ssadgali2@gmail.com>
2024-09-16 10:35:39 +0100

    Req check

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,15 @@
 const displayMessage = require('./0-console');
 
-displayMessage("Welcome to Holberton School, what is your name?");
+displayMessage('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
   const text = process.stdin.read();
 
-  if (text !== null)
+  if (text !== null) {
     process.stdout.write(`Your name is: ${text}`);
+  }
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write("This important software is now closing\n");
+  process.stdout.write('This important software is now closing\n');
 });

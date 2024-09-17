6c1e52751b5d3ee770e7fb29e3f2775d4341ca7f
Soufiane Sadgali <ssadgali2@gmail.com>
2024-09-17 09:15:28 +0100

    Req fix

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -7,24 +7,23 @@ const readDatabase = (path) => new Promise((resolve, reject) => {
   if (!path) {
     reject(new Error('Cannot load the database'));
   }
-  fs.readFile(path, 'utf-8', (err, data) => {
+  fs.readFile((path), 'utf-8', (err, data) => {
     if (err) {
       reject(new Error('Cannot load the database'));
     } else {
       const studentCls = {};
       const ln = data.trim().split('\n');
-      const hdrs = ln[0].split(',');
 
       let i = 1;
       while (i <= (ln.length - 1)) {
         const student = ln[i].split(',');
-	const field = student[student.length - 1];
-	const firstname = student[0];
-	if (!studentCls[field]) {
-	  studentCls[field] = [];
-	}
-	studentCls[field].push(firstname);
-	i += 1;
+        const field = student[student.length - 1];
+        const firstname = student[0];
+        if (!studentCls[field]) {
+          studentCls[field] = [];
+        }
+        studentCls[field].push(firstname);
+        i += 1;
       }
       resolve(studentCls);
     }

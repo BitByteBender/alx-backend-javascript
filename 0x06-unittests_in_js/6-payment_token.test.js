0a695f9d6c10709207e171e3f86afcd98bd23282
Soufiane Sadgali <ssadgali2@gmail.com>
2024-09-18 18:06:22 +0100

    Async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('Returns a resolved promise with the data obj', (done) => {
+    getPaymentTokenFromAPI(true).then((response) => {
+      expect(response).to.deep.equal({ data: 'Successful response from the API' });
+      done();
+    }).catch(done);
+  });
+});

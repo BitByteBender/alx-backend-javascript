8ac6027cd74ff054b428f2ee81be25eff388d4ce
Soufiane Sadgali <ssadgali2@gmail.com>
2024-09-18 18:42:13 +0100

    Testing api tests

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -5,30 +5,14 @@ const expect = chai.expect;
 const devSrv = 'http://localhost:7865';
 
 describe('Request dev server path', () => {
-  let server;
-
-  before((done) => {
-   server = app.listen(7865, 'localhost', () => {
-     console.log('Dev server running on port 7865');
-     done();
-   });
-  });
-
-  after((done) => {
-    server.close(() => {
-      console.log('Dev server stopped');
-      done();
-    });
-  });
-
-  it('Returns status code 200', function(done) {
+  it('Returns status code 200', (done) => {        
     request.get(devSrv, function(err, res, body) {
       expect(res.statusCode).to.equal(200);
       done();
     });
   });
 
-  it('Returns result of the body', function(done) {
+  it('Returns result of the body', (done) => {
     request.get(devSrv, function(err, res, body) {
       expect(body).to.equal('Welcome to the payment system');
       done();
